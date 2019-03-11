63211214c361f80bbde5283ebd8328b8f68d0788
Mohammad Kavousi <mohamad.kav@gmail.com>
2019-03-11 15:15:46 -0500

    Added reducer module

--- src/main/java/edu/nu/forensic/reader/AvroReader.java
+++ src/main/java/edu/nu/forensic/reader/AvroReader.java
@@ -3,8 +3,11 @@ package edu.nu.forensic.reader;
 import com.bbn.tc.schema.SchemaNotInitializedException;
 import com.bbn.tc.schema.avro.cdm19.*;
 import com.bbn.tc.schema.serialization.AvroGenericDeserializer;
+import edu.nu.forensic.util.RecordConverter;
+import org.apache.avro.generic.GenericContainer;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.IndexedRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -14,19 +17,40 @@ import java.io.IOException;
 @Component
 public class AvroReader {
 
+    @Autowired
+    private RecordConverter recordConverter;
+
+
     public void readTrace(File source) throws IOException, SchemaNotInitializedException {
         AvroGenericDeserializer avroGenericDeserializer=new AvroGenericDeserializer("schema/TCCDMDatum.avsc","schema/TCCDMDatum.avsc",
-                false,source);
+                true,source);
         while(true){
-            IndexedRecord data= (IndexedRecord)avroGenericDeserializer.deserializeNextRecordFromFile();
+            GenericContainer data= (GenericContainer)avroGenericDeserializer.deserializeNextRecordFromFile();
             if(data==null)
                 break;
-            GenericData.Fixed uuid=(GenericData.Fixed)data.get(3);
-            TCCDMDatum datum=new TCCDMDatum(data.get(0),(CharSequence)data.get(1), RecordType.valueOf(data.get(2).toString()),new UUID(uuid.bytes()),(Integer)data.get(4),InstrumentationSource.valueOf(data.get(5).toString()));
-            if(datum.getDatum() instanceof Subject)
-                System.out.println(((Subject) datum.getDatum()).getType());
-            else
-                System.out.println(datum.getDatum().getClass());
+            TCCDMDatum CDMdatum=(TCCDMDatum) data;
+            try {
+                if (CDMdatum.getDatum() instanceof Subject)
+                    recordConverter.saveAndConvertBBNSubjectToSubject((Subject) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof Principal)
+                    recordConverter.saveAndConvertBBNPrincipalToPrincipal((Principal) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof FileObject)
+                    recordConverter.saveAndConvertBBNFileObjectToFileObject((FileObject) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof RegistryKeyObject)
+                    recordConverter.saveAndConvertBBNRegistryKeyObjectToRegistryKeyObject((RegistryKeyObject) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof NetFlowObject)
+                    recordConverter.saveAndConvertBBNNetFlowObjectToNetFlowObject((NetFlowObject) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof Event)
+                    recordConverter.saveAndConvertBBNEventToEvent((Event) CDMdatum.getDatum());
+                else if (CDMdatum.getDatum() instanceof UnitDependency)
+                    recordConverter.saveAndConvertBBNUnitDependencyToUnitDependency((UnitDependency) CDMdatum.getDatum());
+                else
+                    System.err.println(CDMdatum.toString());
+            }catch (Exception e){
+                System.err.println("Darn! We have an unknown bug over: ");
+                System.err.println(CDMdatum);
+                e.printStackTrace();
+            }
         }
         avroGenericDeserializer.close();
     }
