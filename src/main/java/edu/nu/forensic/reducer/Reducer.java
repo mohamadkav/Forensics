c99d7497213f744a855d5edb39be89046b652019
Mohammad <mohamad.kav@gmail.com>
2019-05-13 16:10:03 -0500

    How was this even being compiled?

--- src/main/java/edu/nu/forensic/reducer/Reducer.java
+++ src/main/java/edu/nu/forensic/reducer/Reducer.java
@@ -1,23 +1,9 @@
 package edu.nu.forensic.reducer;
 
 
-import com.bbn.tc.schema.avro.cdm19.SubjectType;
-import edu.nu.forensic.db.entity.Event;
-import edu.nu.forensic.db.entity.Subject;
 import edu.nu.forensic.db.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.*;
-
-import static edu.nu.forensic.reducer.FPGrowth.findFrequentItemsetWithSuffix;
-import static edu.nu.forensic.reducer.FSA.buildFSA;
-import static edu.nu.forensic.reducer.StatementRoot.printFSA;
-import static java.util.stream.Collectors.toMap;
 
 
 @Component
